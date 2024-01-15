4c3a6e0681f2ce71ddcf9daaef1f74f0bc2362eb
Fox-sys <>
2024-01-15 18:47:10 +0300

    поправить сторону

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ while ant.step():
     ...
 
 
-image = Image.fromarray((1 - field.get_field()) * 255)
+image = Image.fromarray((field.get_field()) * 255)
 image.save('ant_path.png')
-print(np.sum(field.get_field()))
+print(np.sum(1 - field.get_field()))
